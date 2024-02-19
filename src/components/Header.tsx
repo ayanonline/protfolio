1142174340c86942f26d09a0f2ffdb0ecdb37b2a
ayanonline <ayanghosh1389@gmail.com>
2024-02-19 18:08:25 +0530

    Mobile design started

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,11 +13,18 @@ import {
    Avatar,
    Button,
 } from '@nextui-org/react';
+import { GoHome } from 'react-icons/go';
+import { FaFolderOpen } from 'react-icons/fa6';
+import { CiFileOn, CiFolderOn, CiHome, CiMail } from 'react-icons/ci';
 
 export default function Header() {
    return (
-      <header className="hidden md:block">
-         <Navbar isBordered maxWidth="full" className="px-[40px] py-[12px]">
+      <header className="w-full fixed bottom-0 z-50 md:top-0">
+         <Navbar
+            isBordered
+            maxWidth="full"
+            className="hidden md:block px-[40px] py-[12px]"
+         >
             <NavbarContent justify="start">
                <NavbarBrand className="mr-4">
                   {/* <AcmeLogo /> */}
@@ -45,6 +52,26 @@ export default function Header() {
                </NavbarItem>
             </NavbarContent>
          </Navbar>
+
+         <div className="md:hidden rounded-tl-md rounded-tr-md border bg-white h-[75px] shadow-inner flex justify-around">
+            <div className="flex flex-col justify-center items-center">
+               <CiHome className="h-6 w-6" />
+               <span>Home</span>
+            </div>
+            <div className="flex flex-col justify-center items-center">
+               {/* <FaFolderOpen className="h-6 w-6" /> */}
+               <CiFolderOn className="h-6 w-6" />
+               <span>Projects</span>
+            </div>
+            <div className="flex flex-col justify-center items-center">
+               <CiFileOn className="h-6 w-6" />
+               <span>Resume</span>
+            </div>
+            <div className="flex flex-col justify-center items-center">
+               <CiMail className="h-6 w-6" />
+               <span>Contact</span>
+            </div>
+         </div>
       </header>
    );
 }
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,10 +1,42 @@
 import React from 'react';
+import projects from '@/data/projects.json';
+import {
+   Card,
+   Image,
+   Button,
+   Link,
+   CardHeader,
+   CardBody,
+   CardFooter,
+} from '@nextui-org/react';
+import { useRouter } from 'next/router';
 
 const Projects = () => {
    return (
       <div className="p-4">
          <p className="font-bold text-[22px]">Recent Projects</p>
-         <div></div>
+         <div className="flex flex-wrap gap-3 justify-between">
+            {projects.map((item, index) => (
+               <Card
+                  key={index}
+                  radius="sm"
+                  className="border-none min-w-[165px] max-w-[165px] min-h-[242px] max-h-[242px]"
+               >
+                  <a href={item.url} target="blank">
+                     <img
+                        alt="Woman listing to music"
+                        className="rounded-md object-cover min-h-[173px] max-h-[173px] w-[173px]"
+                        src="https://miro.medium.com/v2/resize:fit:1000/1*KDMx1YspSrBcFJG-NDZgDg.png"
+                     />
+                  </a>
+
+                  <div className="p-1">
+                     <p className="font-bold">{item.title.slice(0, 16)}...</p>
+                     <p>{item.desc.slice(0, 18)}...</p>
+                  </div>
+               </Card>
+            ))}
+         </div>
       </div>
    );
 };
